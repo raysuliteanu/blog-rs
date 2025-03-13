bf0c95019efd69b22e9c903f1d6aa2dba651deca
Ray Suliteanu <raysuliteanu@pm.me>
2025-03-12 17:47:00 -0700

    extract blog/post/user to model module

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,47 +1,9 @@
-use chrono::{DateTime, Utc};
 use diesel::prelude::*;
-use schema::{blogs, posts, users};
+use model::Post;
 
 mod db;
+mod model;
 mod schema;
-
-joinable!(posts -> users (user_id));
-joinable!(posts -> blogs (blog_id));
-
-#[derive(Insertable, Identifiable, Queryable, Selectable, PartialEq, Debug)]
-#[diesel(table_name = users)]
-pub struct User {
-    id: i32,
-    username: String,
-}
-
-#[derive(Debug, Associations, Identifiable, Queryable, Insertable, Selectable)]
-#[diesel(table_name = posts)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-#[diesel(belongs_to(Blog))]
-#[diesel(belongs_to(User))]
-struct Post {
-    id: i32,
-    blog_id: i32,
-    user_id: i32,
-    create_date: DateTime<Utc>,
-    updated_date: DateTime<Utc>,
-    published_date: Option<DateTime<Utc>>,
-    title: String,
-    description: String,
-    content: String,
-}
-
-#[derive(Debug, Identifiable, Queryable, Insertable, Selectable)]
-#[diesel(table_name = blogs)]
-struct Blog {
-    id: i32,
-    user_id: i32,
-    create_date: DateTime<Utc>,
-    updated_date: DateTime<Utc>,
-    title: String,
-    description: String,
-}
 
 fn main() {
     use schema::blogs::dsl::*;
@@ -50,52 +12,21 @@ fn main() {
 
     let mut conn: PgConnection = db::establish_connection();
 
-    let user = User {
-        id: 1,
-        username: "alice".to_string(),
-    };
+    let user = model::get_dummy_user();
 
     diesel::insert_into(users)
         .values(&user)
         .execute(&mut conn)
         .expect("Error saving new user");
 
+    let blog = model::get_dummy_blog();
+
     diesel::insert_into(blogs)
-        .values(&Blog {
-            id: 1,
-            user_id: 1,
-            create_date: Utc::now(),
-            updated_date: Utc::now(),
-            title: "Alice's Blog".to_string(),
-            description: "This is my blog".to_string(),
-        })
+        .values(&blog)
         .execute(&mut conn)
         .expect("Error saving new blog");
 
-    let vec = vec![
-        Post {
-            id: 1,
-            blog_id: 1,
-            user_id: 1,
-            create_date: Utc::now(),
-            updated_date: Utc::now(),
-            published_date: None,
-            title: "Hello, world!".to_string(),
-            description: "This is a test post".to_string(),
-            content: "This is the content of the post".to_string(),
-        },
-        Post {
-            id: 2,
-            blog_id: 1,
-            user_id: 1,
-            create_date: Utc::now(),
-            updated_date: Utc::now(),
-            published_date: None,
-            title: "Hello, world!".to_string(),
-            description: "This is a test post".to_string(),
-            content: "This is the content of the post".to_string(),
-        },
-    ];
+    let vec = model::get_dummy_posts();
     diesel::insert_into(posts)
         .values(&vec)
         .execute(&mut conn)
@@ -105,6 +36,7 @@ fn main() {
         .select(Post::as_select())
         .load::<Post>(&mut conn)
         .unwrap();
+
     for post in results {
         println!("{:?}", post);
     }
---
+++ server/src/model.rs
@@ -0,0 +1,87 @@
+use crate::schema::{blogs, posts, users};
+use chrono::{DateTime, Utc};
+use diesel::prelude::*;
+
+joinable!(blogs -> users (user_id));
+joinable!(posts -> users (user_id));
+joinable!(posts -> blogs (blog_id));
+
+#[derive(Insertable, Identifiable, Queryable, Selectable, PartialEq, Debug)]
+#[diesel(table_name = users)]
+pub struct User {
+    id: i32,
+    username: String,
+}
+
+#[derive(Debug, Associations, Identifiable, Queryable, Insertable, Selectable)]
+#[diesel(table_name = posts)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(belongs_to(Blog))]
+#[diesel(belongs_to(User))]
+pub struct Post {
+    id: i32,
+    blog_id: i32,
+    user_id: i32,
+    create_date: DateTime<Utc>,
+    updated_date: DateTime<Utc>,
+    published_date: Option<DateTime<Utc>>,
+    title: String,
+    description: String,
+    content: String,
+}
+
+#[derive(Debug, Identifiable, Queryable, Insertable, Selectable)]
+#[diesel(table_name = blogs)]
+pub struct Blog {
+    id: i32,
+    user_id: i32,
+    create_date: DateTime<Utc>,
+    updated_date: DateTime<Utc>,
+    title: String,
+    description: String,
+}
+
+pub fn get_dummy_user() -> User {
+    User {
+        id: 1,
+        username: "Alice".to_string(),
+    }
+}
+
+pub fn get_dummy_blog() -> Blog {
+    Blog {
+        id: 1,
+        user_id: 1,
+        create_date: Utc::now(),
+        updated_date: Utc::now(),
+        title: "Alice's Blog".to_string(),
+        description: "This is my blog".to_string(),
+    }
+}
+
+pub fn get_dummy_posts() -> Vec<Post> {
+    vec![
+        Post {
+            id: 1,
+            blog_id: 1,
+            user_id: 1,
+            create_date: Utc::now(),
+            updated_date: Utc::now(),
+            published_date: None,
+            title: "Hello, world!".to_string(),
+            description: "This is a test post".to_string(),
+            content: "This is the content of the post".to_string(),
+        },
+        Post {
+            id: 2,
+            blog_id: 1,
+            user_id: 1,
+            create_date: Utc::now(),
+            updated_date: Utc::now(),
+            published_date: None,
+            title: "Hello, world!".to_string(),
+            description: "This is a test post".to_string(),
+            content: "This is the content of the post".to_string(),
+        },
+    ]
+}
